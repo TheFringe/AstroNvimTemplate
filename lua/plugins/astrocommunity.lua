15234fd7023f2d3ae750861710bcb024cfc6a0c2
Eric <eric@MacBook-Pro-som-tillhor-Eric.local>
2024-08-25 19:50:32 +0200

    LSP for angular devolpment, Theme Nord

---
+++ lua/plugins/astrocommunity.lua
@@ -0,0 +1,7 @@
+return {
+  "AstroNvim/astrocommunity",
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.nord-nvim" },
+  { import = "astrocommunity.note-taking.obsidian-nvim" }
+
+}
